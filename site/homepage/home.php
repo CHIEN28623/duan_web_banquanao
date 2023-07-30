acd8d90b718a0c16f4ed7da87dc52c391aff04ae
brandon <brandonnlee2210@gmail.com>
2023-07-30 19:28:39 +0700

    style: fix style trang tất cả sản phẩm

--- site/homepage/home.php
+++ site/homepage/home.php
@@ -21,26 +21,26 @@ require "search.php";
       extract($item);
       ?>
 
-      <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-        <a href="../product/product-details&id=<?php echo $product_id ?>">
-          <img class="hover:grow hover:shadow-lg" src="/<?= $image ?>">
-          <div class="pt-3 flex items-center justify-between">
-            <p class="font-bold text-xl">
-              <?= $name ?>
-            </p>
-
-          </div>
-          <p class="flex gap-3">
-            <s class="pt-1 text-gray-900">
-              <?= number_format($price) ?> <span class="text-gray-500">VND</span>
-            </s>
-            <span class="pt-1 text-blue-500">
-              <?= number_format($price - $price * $discount / 100) ?> VND
-            </span>
+    <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+      <a href="../product/product-details&id=<?php echo $product_id ?>">
+        <img class="hover:grow hover:shadow-lg" src="/<?= $image ?>">
+        <div class="pt-3 flex items-center justify-between">
+          <p class="font-bold text-xl">
+            <?= $name ?>
           </p>
-        </a>
-      </div>
-      <?php
+
+        </div>
+        <p class="flex gap-3">
+          <s class="pt-1 text-gray-900">
+            <?= number_format($price) ?> <span class="text-gray-500">VND</span>
+          </s>
+          <span class="pt-1 text-blue-500">
+            <?= number_format($price - $price * $discount / 100) ?> VND
+          </span>
+        </p>
+      </a>
+    </div>
+    <?php
     }
     ?>
 
@@ -50,50 +50,50 @@ require "search.php";
 
 <?php foreach ($categories as $category) { ?>
 
-  <section class="bg-white py-8">
+<section class="bg-white py-8">
 
-    <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+  <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
 
-      <nav id="store" class="w-full z-30 top-0 px-6 py-1">
-        <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
+    <nav id="store" class="w-full z-30 top-0 px-6 py-1">
+      <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
 
-          <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
-            <?= $category['name'] ?>
-          </a>
-        </div>
-      </nav>
+        <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
+          <?= $category['name'] ?>
+        </a>
+      </div>
+    </nav>
 
-      <?php
+    <?php
       $items = product_select_by_category($category['category_id']);
       foreach ($items as $item) {
         extract($item);
         ?>
 
-        <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-          <a href="../product?product-details&id=<?php echo $product_id ?>">
-            <img class="hover:grow hover:shadow-lg" src="/duan1/<?= $image ?>">
-            <div class="pt-3 flex items-center justify-between">
-              <p class="font-bold text-xl">
-                <?= $name ?>
-              </p>
+    <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+      <a href="../product?product-details&id=<?php echo $product_id ?>">
+        <img class="hover:grow hover:shadow-lg" src="/<?= $image ?>">
+        <div class="pt-3 flex items-center justify-between">
+          <p class="font-bold text-xl">
+            <?= $name ?>
+          </p>
 
-            </div>
-            <p class="flex gap-3">
-              <s class="pt-1 text-gray-900">
-                <?= number_format($price) ?> <span class="text-gray-500">VND</span>
-              </s>
-              <span class="pt-1 text-blue-500">
-                <?= number_format($price - $price * $discount / 100) ?> VND
-              </span>
-            </p>
-          </a>
         </div>
-        <?php
+        <p class="flex gap-3">
+          <s class="pt-1 text-gray-900">
+            <?= number_format($price) ?> <span class="text-gray-500">VND</span>
+          </s>
+          <span class="pt-1 text-blue-500">
+            <?= number_format($price - $price * $discount / 100) ?> VND
+          </span>
+        </p>
+      </a>
+    </div>
+    <?php
       }
       ?>
 
-    </div>
+  </div>
 
-  </section>
+</section>
 
 <?php } ?>
